036baa906c8a745576af489458a3372370a272da
sudharsan_m <manivannansudharsan542@gmail.com>
2025-12-13 10:21:11 +0530

    Add files via upload

---
+++ Inp & Output prgms/blood grp prg.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+      System.out.print("Enter your Blood Group: ");
+      String s = sc.next();
+       System.out.println("Your Blood Group is : "+s);
+    }
+}
---
+++ Inp & Output prgms/print char for gn ASCII val.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char b = sc.next().charAt(0);
+        int c = (int)b;
+        System.out.println(c);
+    }
+}
