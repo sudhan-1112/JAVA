68974f4f83cbe99cd0177a292092fc7ce8fb5c13
sudharsan_m <manivannansudharsan542@gmail.com>
2025-12-13 10:38:46 +0530

    Add files via upload

---
+++ Inp & Output prgms/roll no & name prg.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      String s = sc.next();
+       System.out.println("Roll No: "+a+","+"Name: "+s);
+    }
+}
