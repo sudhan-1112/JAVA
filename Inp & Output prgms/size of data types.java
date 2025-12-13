89db0f601388fdd15692495239bdbc208fe7e64b
sudharsan_m <manivannansudharsan542@gmail.com>
2025-12-13 10:37:06 +0530

    Add files via upload

---
+++ Inp & Output prgms/size of data types.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        System.out.println("Integer: " + Integer.BYTES);
+        System.out.println("Character: "+ Character.BYTES);
+        System.out.println("Float: "+ Float.BYTES);
+        System.out.println("Double: "+Double.BYTES);
+    }
+}
