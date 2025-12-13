a7a28f7c71e6c06f728bb3538cda8d46113597bf
sudharsan_m <manivannansudharsan542@gmail.com>
2025-12-13 14:29:55 +0530

    Update alphabet or not.java

--- Conditional prgms/alphabet or not.java
+++ Conditional prgms/alphabet or not.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q3 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
        char ch=sc.next().charAt(0);
